9a3dfb20dfe62a2e6dc646b9996dca39719eb299
joseelinchevalay <josselin54.chevalay@gmail.com>
2024-03-27 22:25:50 +0100

    implement some example

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+use proptest::prelude::*;
+use mockall::{automock, mock, predicate::*, Sequence};
+use criterion::{criterion_group, criterion_main, Criterion};
+
+
+#[cfg_attr(test, automock)]
+trait Greeter {
+    fn greet(&self, name: &str) -> String;
+}
+
+
+proptest! {
+    #[test]
+    fn test_string_reversal(s: String) {
+        let reversed = s.chars().rev().collect::<String>();
+        assert_eq!(s, reversed.chars().rev().collect::<String>());
+    }
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -11,4 +30,34 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+
+    #[test]
+    fn test_greeting() {
+        let mut mock = MockGreeter::new();
+
+        mock.expect_greet()
+            .with(mockall::predicate::eq("Alice"))
+            .times(1)
+            .returning(|_| String::from("Hello, Alice"));
+        assert_eq!("Hello, Alice", mock.greet("Alice"));
+    }
+
+    #[test]
+    fn test_greeting_sequence() {
+        let mut seq = Sequence::new();
+        let mut mock = MockGreeter::new();
+        mock.expect_greet()
+            .with(mockall::predicate::eq("Alice"))
+            .times(1)
+            .in_sequence(&mut seq)
+            .returning(|_| String::from("Hello, Alice"));
+        mock.expect_greet()
+            .with(mockall::predicate::eq("Bob"))
+            .times(1)
+            .in_sequence(&mut seq)
+            .returning(|_| String::from("Hello, Bob"));
+        assert_eq!("Hello, Alice", mock.greet("Alice"));
+        assert_eq!("Hello, Bob", mock.greet("Bob"));
+    }
+
 }
